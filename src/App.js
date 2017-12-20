f1f745f1795adfbf3c912398cff33bfbbc3d0013
Travis Kohlbeck <tdkohlbeck+github@gmail.com>
2017-12-19 18:04:32 -0800

    adds list view and datum states

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Button,
   ButtonGroup,
   ButtonToolbar,
+  Dropdown,
   DropdownButton,
   FormControl,
   FormGroup,
@@ -29,13 +30,26 @@ let AddTagButton = (props) => (
   </Button>
 )
 
-let Datum = (props) => (
-  <ButtonToolbar>
-    <Tag />
-    <Tag />
-    <AddTagButton />
-  </ButtonToolbar>
-)
+let Datum = (props) => {
+  let DatumState
+  if (props.mode == 'default') {
+    DatumState = (
+      <ButtonToolbar>
+        <Tag />
+        <Tag />
+      </ButtonToolbar>
+    )
+  } else if (props.mode == 'edit') {
+    DatumState = (
+      <ButtonToolbar>
+        <Tag />
+        <Tag />
+        <AddTagButton />
+      </ButtonToolbar>
+    )
+  }
+  return DatumState
+}
 
 let AddButton = (props) => (
   <Button bsStyle='primary'>
@@ -45,16 +59,104 @@ let AddButton = (props) => (
 
 let DatumBar = (props) => (
   <div id='datum-bar'>
-    <Datum />
+    <Datum mode='edit'/>
     <AddButton />
+    <Custom />
   </div>
 )
 
+class CustomToggle extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+
+    this.props.onClick(e);
+  }
+
+  render() {
+    return (
+      <DropdownButton
+        href=""
+        onClick={this.handleClick}
+      >
+        {this.props.children}
+      </DropdownButton>
+    );
+  }
+}
+
+class CustomMenu extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.onChange = e => this.setState({ value: e.target.value });
+
+    this.state = { value: '' };
+  }
+
+  focusNext() {
+    const input = React.findDOMNode(this.input);
+
+    if (input) {
+      input.focus();
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className="dropdown-menu">
+        <ul className="list-unstyled">
+          {React.Children.toArray(children).filter(child => (
+            !value.trim() || child.props.children.indexOf(value) !== -1
+          ))}
+        </ul>
+        <FormControl
+          ref={(c) => { this.input = c; }}
+          type="text"
+          placeholder="Type to filter..."
+          onChange={this.onChange}
+          value={this.state.value}
+        />
+      </div>
+    );
+  }
+}
+
+const Custom = () => (
+  <Dropdown id="dropdown-custom-menu" dropup>
+    <CustomToggle bsRole="toggle">
+      Custom toggle
+    </CustomToggle>
+
+    <CustomMenu bsRole="menu">
+      <MenuItem eventKey="1">Red</MenuItem>
+      <MenuItem eventKey="2">Blue</MenuItem>
+      <MenuItem eventKey="3" active>Orange</MenuItem>
+      <MenuItem eventKey="1">Red-Orange</MenuItem>
+    </CustomMenu>
+  </Dropdown>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ListGroup>
+          <ListGroupItem>
+            <Datum mode='default'/>
+          </ListGroupItem>
+          <ListGroupItem>
+            <Datum mode='default'/>
+          </ListGroupItem>
+        </ListGroup>
         <Nav bsStyle='pills' id='datum-bar-nav'>
           <NavItem>
             <DatumBar />
--- src/Tag.js
+++ src/Tag.js
@@ -4,9 +4,39 @@ import {
   ButtonGroup,
   DropdownButton,
   MenuItem,
+  FormControl,
 } from 'react-bootstrap'
 
 import './Tag.css'
+
+class TagSearch extends React.Component {
+
+  constructor(props, context) {
+    super(props, context)
+    this.onChange = e => this.setState({ value: e.target.value })
+    this.state = {value: '' }
+  }
+
+  focusNext() {
+    const input = React.findDOMNode(this.input);
+
+    if (input) {
+      input.focus();
+    }
+  }
+
+
+  render() {
+    return (
+      <FormControl
+      type='text'
+      value={this.state.value}
+      placeholder='Yay?'
+      onChange={this.handleChange}
+      />
+    )
+  }
+}
 
 const Tag = (props) => (
   <ButtonGroup>
@@ -25,6 +55,13 @@ const Tag = (props) => (
     >
       <MenuItem eventKey="1">one value</MenuItem>
       <MenuItem eventKey="2">another value</MenuItem>
+      <MenuItem eventKey="3">
+        <FormControl
+          ref = { (c) => { this.input = c; } }
+          type='text'
+          placeholder='yey?'
+        />
+      </MenuItem>
     </DropdownButton>
   </ButtonGroup>
 );
